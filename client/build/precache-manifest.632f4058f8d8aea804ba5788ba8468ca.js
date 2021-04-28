274940a88beaeb1f26e1b681b63149240808332f
Amanda <awiehrs.photo@gmail.com>
2021-04-27 21:22:06 -0400

    deploy

---
+++ client/build/precache-manifest.632f4058f8d8aea804ba5788ba8468ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e5ced16f9dfcd1f247b329875de39a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7fe2b7a616deceac3b1",
+    "url": "/static/css/main.69294737.chunk.css"
+  },
+  {
+    "revision": "522c6da5c261361c6d71",
+    "url": "/static/js/2.491aa3f2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.491aa3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7fe2b7a616deceac3b1",
+    "url": "/static/js/main.2fd8b691.chunk.js"
+  },
+  {
+    "revision": "226fd4a5de4b477d8cce",
+    "url": "/static/js/runtime-main.6ff082a3.js"
+  }
+]);
--- server.js
+++ server.js
@@ -19,6 +19,20 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactgooglebooks", { useNewUrlParser: true, useUnifiedTopology: true });
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Books',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`API Server now listening on PORT ${PORT}`);
